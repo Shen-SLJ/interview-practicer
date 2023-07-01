837ddd5bbd01b36590a2a15af475253c4b8a35f3
sjia0047 <sjia0047@student.monash.edu>
2023-07-01 16:42:42 +1000

    feat: allow user to load from previous generated audio

--- run.py
+++ run.py
@@ -7,19 +7,30 @@ import glob
 if __name__ == '__main__':
     audio_ids = []
 
-    # initialisation - generate audio
+    # initialisation - determine load from previous audio/generate new audio
     regenerate = False
     if not os.path.exists("./audio"):
         os.mkdir("./audio")
         regenerate = True
     else:
-        input = ("Regenerate audio (y/n)?")
-        if input.lower() == "y":
+        ans = input("Regenerate audio (y/n)? ")
+        if ans.lower() == "y":
             for path in glob.glob("./audio/*"):
                 os.remove(path)
-            os.rmdir("./audio")
             regenerate = True
+        else:
+            try:
+                audio_ids_f = open("ids.txt", "r")
+                str_ids = audio_ids_f.read().splitlines()
+                audio_ids = [int(id) for id in str_ids]
+            except FileNotFoundError:
+                print("ids.txt file is missing.")
+                regenerate = True # need to regenerate if no id file
+
     if regenerate:
+        print("Generating audio. Please wait.")
+
+        # generate audio files
         f = open("questions.txt", "r")
         lines = f.read().splitlines()
         for i, line in enumerate(lines):
@@ -27,6 +38,11 @@ if __name__ == '__main__':
             tts.save(f"./audio/audio{i}.mp3")
             audio_ids.append(i)
         f.close()
+
+        # write for potential next time loading
+        id_store = open("ids.txt", "w")
+        for id in audio_ids:
+            id_store.write(f"{id}\n")
 
     # play audio
     while len(audio_ids) > 0:
